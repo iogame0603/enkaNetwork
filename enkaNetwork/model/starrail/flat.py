aa33fd9e2311822ad7a8505f55553221c0f166ae
iogame <young312492@gmail.com>
2024-04-16 09:22:31 +0900

    flat 수정

--- enkaNetwork/model/starrail/flat.py
+++ enkaNetwork/model/starrail/flat.py
@@ -3,16 +3,44 @@ from typing import List, Union
 
 from ...util import HonkaiStarrailUtil
 
-class Prop(BaseModel):
+class BasePropValue(BaseModel):
     type: str
+
+    def __init__(self, **data):
+        data["type"] = HonkaiStarrailUtil.get_localizations(data["type"])
+        super().__init__(**data)
+
+class DeltaValue(BasePropValue):
     value: Union[int, float]
+
+    @field_validator("value")
+    def validator_value(cls, value: Union[int, float]):
+        return int(round(value))
+
+    class Config:
+        validate_assignment = True
+
+class PercentValue(BasePropValue):
+    value: float
+
+    @field_validator("value")
+    def validator_value(cls, value: float):
+        return round(value * 100, 1)
+
+    def value2percent(self):
+        return f"{self.value}%"
+
+    class Config:
+        validate_assignment = True
+
+class Prop(BaseModel):
+    propValue: Union[DeltaValue, PercentValue]
 
     def __init__(self, **data):
         if data["type"].find("Delta") != -1:
-            data["value"] = int(round(data["value"]))
+            data["propValue"] = DeltaValue(**data)
         else:
-            data["value"] = round(data["value"] * 100, 1)
-        data["type"] = HonkaiStarrailUtil.get_localizations(data["type"])
+            data["propValue"] = PercentValue(**data)
         super().__init__(**data)
 
 class EquipFlat(BaseModel):
