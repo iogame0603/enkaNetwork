d452c2a41d270c4859ab867e481e5240e881bf1f
iogame <young312492@gmail.com>
2024-04-26 16:20:46 +0900

    setup.py 수정

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="enkaNetwork",
-    version="0.22",
+    version="0.221",
     description="enka network to json library",
     install_requires=[
         "aiofiles==23.2.1",
